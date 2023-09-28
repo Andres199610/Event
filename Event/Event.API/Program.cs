ce3e128e1fb27a8970ed469346fe61e357601fb2
ANDRES FELIPE RAMIREZ USUGA <andresramirez229848@correo.itm.edu.co>
2023-09-28 10:35:53 -0500

    ADD Irepository and Repository

--- Event/Event.API/Program.cs
+++ Event/Event.API/Program.cs
@@ -1,7 +1,7 @@
 using Event.API.Data;
-
+using Event.WEB.Repositories;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.EntityFrameworkCore.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IRepository, Repository > ();
 builder.Services.AddDbContext<DataContext>(x=>x.UseSqlServer("name=DefaultConnection"));
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7294") });
+
+
+
 
 
 var app = builder.Build();
--- Event/Event.WEB/Repositories/Repository.cs
+++ Event/Event.WEB/Repositories/Repository.cs
@@ -1,10 +1,10 @@
 ﻿using System.Text.Json;
 using System.Text;
-
+using System.Net.Http;
 
 namespace Event.WEB.Repositories
 {
-    public class Repository: IRepository
+    public class Repository : IRepository
     {
         private readonly HttpClient _httpClient;
 
@@ -44,7 +44,6 @@ namespace Event.WEB.Repositories
             var responseHttp = await _httpClient.PostAsync(url, messageContet);
             return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
         }
-
         public async Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model)
         {
             var messageJSON = JsonSerializer.Serialize(model);
@@ -64,37 +63,6 @@ namespace Event.WEB.Repositories
             return JsonSerializer.Deserialize<T>(respuestaString, jsonSerializerOptions)!;
         }
 
-        public async Task<HttpResponseWrapper<object>> Delete(string url)
-        {
-            var responseHTTP = await _httpClient.DeleteAsync(url);
-            return new HttpResponseWrapper<object>(null, !responseHTTP.IsSuccessStatusCode, responseHTTP);
 
-        }
-
-        public async Task<HttpResponseWrapper<object>> Put<T>(string url, T model)
-        {
-            var messageJSON = JsonSerializer.Serialize(model);
-            var messageContent = new StringContent(messageJSON, Encoding.UTF8, "application/json");
-            var responseHttp = await _httpClient.PutAsync(url, messageContent);
-            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
-
-        }
-
-        public async Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model)
-        {
-            var messageJSON = JsonSerializer.Serialize(model);
-            var messageContent = new StringContent(messageJSON, Encoding.UTF8, "application/json");
-            var responseHttp = await _httpClient.PutAsync(url, messageContent);
-            if (responseHttp.IsSuccessStatusCode)
-            {
-                var response = await UnserializeAnswer<TResponse>(responseHttp, _jsonDefaultOptions);
-                return new HttpResponseWrapper<TResponse>(response, false, responseHttp);
-            }
-
-            return new HttpResponseWrapper<TResponse>(default, !responseHttp.IsSuccessStatusCode, responseHttp);
-
-        }
-    
-
-}
+    }
 }
