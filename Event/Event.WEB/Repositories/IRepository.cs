90b4af3e07466dcfcf7c0a2e0c7618bbd30077ef
ANDRES FELIPE RAMIREZ USUGA <andresramirez229848@correo.itm.edu.co>
2023-09-28 19:05:28 -0500

    It has updated Repository

--- Event/Event.WEB/Repositories/IRepository.cs
+++ Event/Event.WEB/Repositories/IRepository.cs
@@ -7,7 +7,11 @@
         Task<HttpResponseWrapper<object>> Get(string url);
         Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
 
-       
+        Task<HttpResponseWrapper<object>> Put<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model);
+
+        Task<HttpResponseWrapper<object>> Delete(string url);
 
     }
 }
